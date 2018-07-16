7ad77539a4631eed0359f1c01e677aa2f7a7b05a
Davide Borsatto <davide.borsatto@gmail.com>
2018-07-16 12:01:41 +0200

    chore(readme): Update readme and CI

---
+++ scripts/create-redirector.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the contentful/laravel package.
+ *
+ * @copyright 2015-2018 Contentful GmbH
+ * @license   MIT
+ */
+$travisRepoSlug = \getenv('TRAVIS_REPO_SLUG');
+$indexFile = $argv[1];
+
+$shellOutput = \shell_exec('git tag');
+$tags = \explode("\n", $shellOutput);
+$tags = \array_filter($tags, function ($tag) {
+    return '' !== \trim($tag);
+});
+
+// We remove all non-stable versions from the list as we don't want to direct the docs to them by default
+$tags = \array_filter($tags, function ($tag) {
+    return false === \mb_strpos($tag, '-');
+});
+
+\usort($tags, function ($a, $b) {
+    return \version_compare($b, $a);
+});
+
+\array_push($tags, 'master');
+
+$newestTag = $tags[0];
+$repoParts = \explode('/', $travisRepoSlug);
+$repoOwner = $repoParts[0];
+$repoName = $repoParts[1];
+
+$html = '<meta http-equiv="refresh" content="0; url=https://'.$repoOwner.'.github.io/'.$repoName.'/api/'.$newestTag.'" />';
+
+\file_put_contents($indexFile, $html);
+
+echo 'Created index file redirecting to '.$newestTag.'.'."\n";
