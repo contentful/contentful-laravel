c91f28095073a1f622c1287aeaa0120a672d4a78
Paul Grigoruta <paul.grigoruta@gmail.com>
2020-05-12 16:50:56 +0300

    Configs are now stored in a more Laravel-friendly way.

--- src/config/contentful.php
+++ src/config/contentful.php
@@ -13,36 +13,38 @@ use Contentful\Delivery\ClientOptions;
 use Illuminate\Foundation\Application;
 
 return [
-    /*
-     * The ID of the space you want to access.
-     */
-    'delivery.space' => env('CONTENTFUL_SPACE_ID'),
+    'delivery' => [
+        /*
+         * The ID of the space you want to access.
+         */
+        'space' => env('CONTENTFUL_SPACE_ID'),
 
-    /*
-     * The ID of the environment you want to access.
-     */
-    'delivery.environment' => env('CONTENTFUL_ENVIRONMENT_ID', 'master'),
+        /*
+         * The ID of the environment you want to access.
+         */
+        'environment' => env('CONTENTFUL_ENVIRONMENT_ID', 'master'),
 
-    /*
-     * An API key for the above specified space.
-     */
-    'delivery.token' => env('CONTENTFUL_DELIVERY_TOKEN'),
+        /*
+         * An API key for the above specified space.
+         */
+        'token' => env('CONTENTFUL_DELIVERY_TOKEN'),
 
-    /*
-     * Controls whether Contentful's Delivery or Preview API is accessed.
-     */
-    'delivery.preview' => (bool) env('CONTENTFUL_USE_PREVIEW', false),
+        /*
+         * Controls whether Contentful's Delivery or Preview API is accessed.
+         */
+        'preview' => (bool) env('CONTENTFUL_USE_PREVIEW', false),
 
-    /*
-     * The default locale to use when querying the API.
-     */
-    'delivery.defaultLocale' => env('CONTENTFUL_DEFAULT_LOCALE'),
+        /*
+         * The default locale to use when querying the API.
+         */
+        'defaultLocale' => env('CONTENTFUL_DEFAULT_LOCALE'),
 
-    /*
-     * A closure which manipulates a ClientOptions object.
-     * See Contentful\Delivery\ClientOptions for more.
-     */
-    'delivery.options' => function (ClientOptions $options, Application $application) {
-        // Update $options however you prefer
-    },
+        /*
+         * A closure which manipulates a ClientOptions object.
+         * See Contentful\Delivery\ClientOptions for more.
+         */
+        'options' => function (ClientOptions $options, Application $application) {
+            // Update $options however you prefer
+        },
+    ],
 ];
